f939bb3a6e7975ff6da8d297961a33ac0ab4ca26
tlot217 <tlot217@aucklanduni.ac.nz>
2016-09-20 07:27:12 +0000

    jQuery

---
+++ eureka.js
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<title>Button Magic</title>
+        <link rel='stylesheet' type='text/css' href='stylesheet.css'/>
+	</head>
+	<body>
+     <div><br/><strong>Click Me!</strong></div>   
+	</body>
+</html>
